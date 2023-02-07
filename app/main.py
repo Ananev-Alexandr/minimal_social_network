0caf2ae9f4cd692c4325b10c4aa26ea83fdb3654
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-02-07 17:47:51 +0300

    [NF] # include_router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import users
+from routers import users, posts
 
 app = FastAPI()
 
 
 app.include_router(users.router)
+app.include_router(posts.router)
 
 
 if __name__ == "__main__":
