d06594d52cf82cd5ee2a84de6606e0eb38807b99
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-02-16 15:59:53 +0300

    [NF] #New response model for filters

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -4,13 +4,13 @@ from typing import Union
 
 class UserBase(BaseModel):
     login: str
-    
-    
+
+
 class UserOut(BaseModel):
     first_name: str
     second_name: str
     id: int
-    
+
     class Config:
         orm_mode = True
 
@@ -19,20 +19,20 @@ class UserCreate(UserBase):
     first_name: str
     second_name: str
     password: str
-    
-    
+
+
 class UserLogin(UserBase):
     password: str
-    
+
 
 class User(UserCreate):
     login: str
     id: int
-    
+
     class Config:
         orm_mode = True
-        
-        
+
+
 class PostIn(BaseModel):
     content: str
 
@@ -40,16 +40,23 @@ class PostIn(BaseModel):
 class PostDB(PostIn):
     id: int
     publication_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class FilterPostDB(BaseModel):
+    Post: PostDB
+    User: UserOut
+    likes_on_the_post: int
     
     class Config:
         orm_mode = True
-    
-    
+
+
 class DateFilter(BaseModel):
     start_date: Union[date, None] = None
     end_date: Union[date, None] = None
-
-
 
 
 class FilteredPosts(BaseModel):
@@ -58,27 +65,30 @@ class FilteredPosts(BaseModel):
     publication_date: Union[DateFilter, None] = None
     first_name: Union[str, None] = None
     second_name: Union[str, None] = None
-        
+
+
 class SortPost(BaseModel):
     group_by: Union[str, None] = "asc"
-    sort_by: Union[str, None] = "id"
-    
+    sort_by: Union[str, None] = "publication_date"
+
+
 class Asda(BaseModel):
     filters: Union[FilteredPosts, None] = None
     group: Union[SortPost, None] = None
-    
+
+
 class LikePost(BaseModel):
     post_id: int
-    
-        
-        
+
+
+
 class LikePostPath(LikePost):
     user_id: int
-    
-    
+
+
 class LikePostDB(LikePost):
     id: int
     user_id: int
-    
+
     class Config:
         orm_mode = True
